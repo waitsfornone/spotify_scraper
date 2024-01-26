ef9f46b26075da3aae356beb6aec70596441f696
Tim Enders <enders.tim@gmail.com>
2024-01-25 19:58:07 -0600

    not pulling the start_date properly

--- scrape_plays.py
+++ scrape_plays.py
@@ -17,12 +17,13 @@ def get_recent_tracks(after_date):
 @flow
 def scrape_plays(full_load=False):
     if full_load:
-        start_date = String.load("backfill-start")
+        start_date = String.load("backfill-start").value()
     else:
-        start_date = String.load("current-timestamp")
-    get_recent_tracks(start_date)
+        start_date = String.load("current-timestamp").value()
+    print(start_date)
+    # get_recent_tracks(start_date)
 
 
 if __name__ == "__main__":
     plays = scrape_plays()
-    print(plays['items'])
+    # print(plays['items'])
