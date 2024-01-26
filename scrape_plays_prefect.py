5922054b04565db647ccc70f2f197702432d9398
Tim Enders <enders.tim@gmail.com>
2024-01-25 20:43:53 -0600

    Prefect cant seem to auth right to Spotify. Rewriting as a manual script to scrape and push raw data to B2

---
+++ scrape_plays_prefect.py
@@ -0,0 +1,29 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from prefect import flow, task
+from prefect.blocks.system import String
+
+
+@task
+def get_recent_tracks(after_date):
+    scope = 'user-read-recently-played'
+    spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    results = spotify.current_user_recently_played(after=after_date)
+    print(results)
+
+    return results
+
+
+@flow
+def scrape_plays(full_load=False):
+    if full_load:
+        start_date = String.load("backfill-start").value
+    else:
+        start_date = String.load("current-timestamp").value
+    get_recent_tracks(start_date)
+
+
+if __name__ == "__main__":
+    plays = scrape_plays()
+    print(plays['items'])
